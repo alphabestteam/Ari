471f12ffc0dc90da9cbad7d35541ba4983fb0af8
ariwiesner <ari.w.05122@gmail.com>
2023-09-13 11:30:39 +0300

    fixed json

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import json
 from costumer import Costumer
 from product import Product
 from register import Register
+
+
+def to_json(data: dict, num_of_costumer: int, costumer: Costumer):
+    name = costumer.name_of_costumer
+    cart = [product for product in costumer.shopping_list]
+    total = costumer.total_amount
+    data["summery"].append({"name": name, "cart": cart, "total": total })
 
 def main():
     registers = [Register() for _ in range(1,4)]
@@ -9,6 +17,8 @@ def main():
     client = Costumer(name)
 
     while user_input != 3:
+        data = {"summery": []}
+        num_of_costumer = 1
         if user_input == 1:
             name_of_product = input("enter the product name\n")
             price = int(input("enter the product price\n"))
@@ -26,6 +36,8 @@ def main():
         elif user_input == 4:
             choose_register = int(input("Choose your register between 1 - 4\n"))
             registers[choose_register].checkout_customer(client)
+            to_json(data, num_of_costumer, client)
+            num_of_costumer += 1
             name = input("To start a new shopping enter you name\n")
             client = Costumer(name)
         user_input = int(input("to add product enter - 1, to remove - 2, to quit enter - 3, to end the shopping and start a new one enter - 4\n"))
@@ -36,6 +48,10 @@ def main():
 
     for _ in range(len(registers)):
         print(registers[_])
+
+    with open("shopping_after_running.JSON", 'w') as file:
+        json.dump(data, file)
+
 
 if __name__ == '__main__':
     main()
