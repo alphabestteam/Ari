f26fc97eeb51f0e1ecf6ba08f993735842a30eec
ariwiesner <ari.w.05122@gmail.com>
2023-11-01 18:46:29 +0200

    finished pdf 4 - js

---
+++ pdf3.js
@@ -0,0 +1,12 @@
+function findSeashellsIndicies(target, values) {
+    for (const number of values) {
+        check = target - number
+        if (values.includes(check)) { 
+        return [values.indexOf(number), values.indexOf(check)]
+        } else {
+            return []
+        }
+    }
+};
+//  for example:
+let calculate = findSeashellsIndicies(30, [25, 21, 15, 10, 5])
