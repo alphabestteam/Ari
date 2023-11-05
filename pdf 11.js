4f8296cc2cb2e40dc89183be5cf2e30ec34eb87e
ariwiesner <ari.w.05122@gmail.com>
2023-11-05 08:38:46 +0200

    not finished yet!

---
+++ pdf 11.js
@@ -0,0 +1,37 @@
+// question 1;
+function helloWorld () {
+    return "Hello World";
+}
+
+// question 2;
+function helloName (name) {
+    return "hello " + name
+}
+
+// question 3;
+const sqrNum = (num) => {
+    return num ** 2
+}
+
+// question 4;
+const rectangle = (length, width) => {
+    return length*width
+}
+
+// question 5;
+
+
+// question 6;
+
+
+// question 7;
+
+
+// question 8;
+
+
+// question 9;
+
+
+
+
