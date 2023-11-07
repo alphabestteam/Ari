eeab4d2394588bc0aa49264b8cce480dd7dd6562
ariwiesner <ari.w.05122@gmail.com>
2023-11-07 12:45:42 +0200

    fix bug in pdf 2 - js!

--- pdf2.js
+++ pdf2.js
@@ -1,6 +1,6 @@
 const isSufficientFuel = (distance, literPerKm, fuelLeft) => {
-const requireFoul = distance * literPerKm
-      return requireFoul- fuelLeft > 0
-// this will return true if we have enough fuel and false if we haven't
-};
-  
+      return fuelLeft / literPerKm >= distance;
+      // this will return true if we have enough fuel and false if we haven't
+}
+
+console.log(isSufficientFuel(10,50,75))
