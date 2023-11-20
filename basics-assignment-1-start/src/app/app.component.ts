d7dcadbbafa2da44b6abd83d3d01c7d24d5b46e0
ariwiesner <ari.w.05122@gmail.com>
2023-11-20 11:52:30 +0200

    not finished yet!

---
+++ basics-assignment-1-start/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  isSuccess: boolean = true;
+  onClick() {
+    this.isSuccess = !this.isSuccess
+  }
+
+  clicked: boolean = false;
+  btnText = "Button";
+  changeText() {
+    if (this.clicked) {
+      this.btnText = "Show warning alert!"
+      this.clicked = !this.clicked
+    } else {
+      this.btnText = "Show success alert!"
+      this.clicked = !this.clicked
+    }
+  }
+}
+
